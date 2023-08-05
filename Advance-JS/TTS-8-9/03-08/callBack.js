492b4231d5dd228c258661f145db7b489b0fc3db
Urvish Patel <uspatel199@gmail.com>
2023-08-05 09:56:32 +0530

    add data

--- Advance-JS/TTS-8-9/03-08/callBack.js
+++ Advance-JS/TTS-8-9/03-08/callBack.js
@@ -1,14 +1,15 @@
 function fun1(callBack) {
   setTimeout(() => {
     console.log("==1==>");
-    callBack();
+
+    callBack(err, res);
   }, 2000);
 }
 
 function fun2(params) {
   console.log("==2==>");
 }
-// fun1(() => {
+// fun1((err,res) => {
 //     console.log("==2==>");
 //   });
 
