4d258f8762649c467b49a24b0ca661eb11bc5832
Urvish Patel <uspatel199@gmail.com>
2023-12-13 09:33:56 +0530

    add data

--- Advance-JS/MWF(2)-8-9/0000-Task/pattern.js
+++ Advance-JS/MWF(2)-8-9/0000-Task/pattern.js
@@ -1,16 +1,46 @@
+/*
+
 1;
 12;
 123;
 1234;
 12345;
 
+
+let arr =  "abcdefghijklmnopqrstuvwxyz".split("")
+console.log("-----------  arr----------->", arr)
 let x = 0;
 for (let i = 0; i <= 5; i++) {
   let str = "";
-  for (let j = 1; j <= i; j++) {
-    str += `${x}`;
+  for (let j = 0; j <= i; j++) {
+    str += `${arr[j]}`;
     x++;
   }
   console.log(str);
   str = "";
+}
+*/
+
+let no = ["11423424489", "1342142354234"];
+let x = 0;
+for (let i = 0; i < no.length; i++) {
+  let currentNo = +no[i];
+  let afterOne = +no[i + 1];
+  let afterTwo = +no[i + 2];
+  let afterThree = +no[i + 3];
+
+  let conditioOne = x === 2;
+  //   let conditioTwo = currentNo === afterOne && afterOne === afterTwo;
+  let conditioTwo = false;
+  let conditioThree =
+    afterOne === currentNo + 1 &&
+    afterTwo === currentNo + 2 &&
+    afterThree === currentNo + 3;
+
+  if (conditioOne || conditioTwo || conditioThree) {
+    console.log("No is Unique");
+    break;
+  }
+
+  if (no[i] === no[i + 1]) x++;
 }
