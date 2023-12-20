34740aa3b9d14a8d903bf527434195051436c27f
Urvish Patel <uspatel199@gmail.com>
2023-12-20 08:58:19 +0530

    add data

--- Advance-JS/MWF(2)-8-9/0000-Task/pattern.js
+++ Advance-JS/MWF(2)-8-9/0000-Task/pattern.js
@@ -44,3 +44,73 @@ for (let i = 0; i < no.length; i++) {
 
   if (no[i] === no[i + 1]) x++;
 }
+
+const classResult = [
+  {
+    name: "Vinay",
+    marks: [
+      {
+        subject: "Hindi",
+        marks: 81,
+      },
+      {
+        subject: "English",
+        marks: 50,
+      },
+      {
+        subject: "Gujarati",
+        marks: 92,
+      },
+      {
+        subject: "Maths",
+        marks: 88,
+      },
+    ],
+  },
+  {
+    name: "Sanjay",
+    marks: [
+      {
+        subject: "Hindi",
+        marks: 33,
+      },
+      {
+        subject: "English",
+        marks: 77,
+      },
+      {
+        subject: "Gujarati",
+        marks: 99,
+      },
+      {
+        subject: "Maths",
+        marks: 97,
+      },
+    ],
+  },
+  {
+    name: "Tushar",
+    marks: [
+      {
+        subject: "Hindi",
+        marks: 91,
+      },
+      {
+        subject: "English",
+        marks: 88,
+      },
+      {
+        subject: "Gujarati",
+        marks: 97,
+      },
+      {
+        subject: "Maths",
+        marks: 99,
+      },
+    ],
+  },
+];
+
+// find all students percentage
+// give hiegest percentage holder name
+// give who have heigest percentage subject vise
