33a0e21ae54c800a3dcaf8ef03a73bd3874ae590
Urvish Patel <uspatel199@gmail.com>
2024-02-17 15:06:25 +0530

    add data

---
+++ Advance-JS/TTS-2/02-17/operator.js
@@ -0,0 +1,36 @@
+/*
+// "" -- null -- undefined - 0 - false => falsy value
+let m = "10";
+let m2 = 10;
+
+console.log("----------- m == m2----------->", m == m2);
+console.log("----------- m === m2 ----------->", m === m2);
+*/
+
+/*
+if (10 == 10 || 20 == 10) {
+  console.log("---1-->");
+}
+if (true || true) {
+  console.log("---2-->");
+}
+
+if (true && false) {
+  console.log("---3-->");
+}
+if (true && true) {
+  console.log("---4-->");
+}
+
+ t || f = t
+ f || f = f
+ f && t = f
+ t && t = t
+*/
+
+let x = "test"; // "" / null / undefined / 0 /false
+if (x || x === 0) {
+  console.log("-----------  x----------->", x);
+} else {
+  console.log("nothing have x");
+}
