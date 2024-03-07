469a38b6817c4a00252174d860e6f43e0891eb44
Urvish Patel <uspatel199@gmail.com>
2024-03-07 14:45:53 +0530

    add data

---
+++ Advance-JS/TTS-2/03-07/callback.js
@@ -0,0 +1,22 @@
+function fun1(callBack) {
+  console.log("-----------  callBack----------->", callBack);
+  console.log("-----fun1-----");
+  callBack();
+}
+
+// function fun2(params) {
+//   console.log("-----fun2-----");
+// }
+
+// fun1(fun2);
+
+fun1(function () {
+  console.log("-----fun2-----");
+});
+
+// call back hell -> nexted call back 
+// fun1(() => {
+//   () => {
+//     () => {};
+//   };
+// });
