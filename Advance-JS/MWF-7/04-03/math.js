ec107154557c7fa1f81118637415298262c627fd
Urvish Patel <uspatel199@gmail.com>
2024-04-03 07:58:00 +0530

    add\

---
+++ Advance-JS/MWF-7/04-03/math.js
@@ -0,0 +1,41 @@
+/*
+// ceil()	It returns a smallest integer value, greater than or equal to the given number.
+
+console.log("ceil()----->", Math.ceil(12.23));
+console.log("ceil()----->", Math.ceil(12.0));
+
+// floor()	It returns largest integer value, lower than or equal to the given number.
+console.log("floor()----->", Math.floor(-12.23));
+console.log("floor()----->", Math.floor(-12.0));
+console.log("floor()----->", Math.floor(-12.99));
+
+
+// trunc()	It returns an integer part of the given number.
+console.log("trunc()----->", Math.trunc(-12.23));
+console.log("trunc()----->", Math.trunc(-12.0));
+console.log("trunc()----->", Math.trunc(-12.99));
+
+
+// max()	It returns maximum value of the given numbers.
+let arr = [-12.9, -11, 1, 2, 45, 7, 8, 9];
+console.log("max()----->", Math.max(...arr));
+
+// min()	It returns minimum value of the given numbers.
+console.log("min()----->", Math.min(-12.9, -11, 1, 2, 45, 7, 8, 9));
+
+
+// round()	It returns closest integer value of the given number.
+
+console.log("round()----->", Math.round(12.0));
+console.log("round()----->", Math.round(12.2));
+console.log("round()----->", Math.round(12.4));
+console.log("round()----->", Math.round(12.5));
+console.log("round()----->", Math.round(12.9));
+
+*/
+
+// random()	It returns random number between 0 (inclusive) and 1 (exclusive).
+
+console.log("random()----->", Math.random());
+console.log("random()----->", Math.random());
+console.log("random()----->", Math.random());
