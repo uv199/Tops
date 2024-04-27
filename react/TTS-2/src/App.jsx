fa4b15ceee66e2eadd7bad66dd3a4f22e6b8b821
Urvish Patel <uspatel199@gmail.com>
2024-04-27 13:57:51 +0530

    add

--- react/TTS-2/src/App.jsx
+++ react/TTS-2/src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import FunctionalCom from "./1.componenet/FunctionalCom";
 import HeaderCom from "./1.componenet/HeaderCom";
+import FunctionalState from "./2.State/FunctionalState";
 
 export default function App() {
   return (
     <div>
-      <HeaderCom />
+      {/* <HeaderCom /> */}
       <div>
-        <FunctionalCom />
-        <FunctionalCom />
-        <FunctionalCom />
-        <FunctionalCom />
-        <FunctionalCom />
-        <FunctionalCom />
+        {/* <FunctionalCom /> */}
+        <FunctionalState />
       </div>
     </div>
   );
