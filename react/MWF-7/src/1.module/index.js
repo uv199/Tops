6e9c42e0078449abbb0303538c0ed7f7298aef2e
Urvish Patel <uspatel199@gmail.com>
2024-04-29 08:14:15 +0530

    add

---
+++ react/MWF-7/src/1.module/index.js
@@ -0,0 +1,10 @@
+import testName, { age } from "./fun2.js";
+console.log("-----------  age----------->", age);
+
+// console.log("-----------  testName----------->", testName);
+// testName();
+
+import { fun1One, fun1Two, name } from "./fun1.js";
+console.log("-----------  name----------->", name);
+console.log("-----------  fun1Two----------->", fun1Two);
+console.log("-----------  fun1One----------->", fun1One);
---
+++ react/MWF-7/src/main.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+let head = <h1>Header</h1>;
+
+let nameArr = ["uv", "js", "ts"];
+
+let user = { name: "uv", age: 23, hobby: [1, 2, 3, 4] };
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>5 + 5 </h1>
+    <h1>{5 + 5}</h1>
+    {head}
+
+    <hr />
+
+    <h1>name : {user.name}</h1>
+    <h1>age : {user.name}</h1>
+
+    <hr />
+
+    {nameArr.map((e) => {
+      return <h1>{e}</h1>;
+    })}
+    <hr />
+
+    <h1>{nameArr[0]}</h1>
+    <h1>{nameArr[1]}</h1>
+    <h1>{nameArr[2]}</h1>
+  </React.StrictMode>
+);
