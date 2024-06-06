9ba06eaf3a254b0ca259de028b831cd8c6d25e7f
Urvish Patel <uspatel199@gmail.com>
2024-06-06 20:05:21 +0530

    add

---
+++ Advance-JS/Raj/06-06/math.js
@@ -0,0 +1,50 @@
+/*
+// ceil()	It returns a smallest integer value, greater than or equal to the given number.
+
+// let ans = Math.ceil(2.1);
+let ans = Math.ceil(2.0);
+console.log("🚀 ~ ans:", ans);
+
+
+// floor()	It returns largest integer value, lower than or equal to the given number.
+
+let ans2 = Math.floor(2.99);
+// let ans2 = Math.floor(2.1);
+console.log("🚀 ~ ans2:", ans2);
+
+// round()  It returns a number, rounded to the nearest integer.
+
+let ans3 = Math.round(2.5);
+// let ans3 = Math.round(2.4);
+// let ans3 = Math.round(2.5);
+console.log("🚀 ~ ans3:", ans3);
+
+
+// sqrt()  It returns the square root of a number.
+
+let ans4 = Math.sqrt(4);
+console.log("🚀 ~ ans4:", ans4);
+
+let ans5 = Math.sqrt(9);
+console.log("🚀 ~ ans5:", ans5);
+
+// max()	It returns maximum value of the given numbers.
+let arr = [1, 2, 3, 4, 1, 2, 0, 1, 4.1, 1, 4];
+// let max = Math.max(1, 2, 3, 4, 1, 2, 0, 1, 4.1, 1, 4);
+let max = Math.max(...arr);
+
+console.log("🚀 ~ max:", max);
+
+// min()	It returns minimum value of the given numbers.
+
+let min = Math.min(1, 2, 3, 4, 1, 2, 0, 1, 0.1, -1, 4.1, 1, 4);
+console.log("�� ~ min:", min);
+
+
+// random()	It returns random number between 0 (inclusive) and 1 (exclusive).
+
+let random = Math.random() * 1000000;
+console.log("🚀 ~ random:", Math.trunc(random));
+*/
+
+// generate a random number between 1  to 6, 1 and 6 both are inclusive 
---
+++ Advance-JS/Raj/06-06/number.js
@@ -0,0 +1,18 @@
+/*
+// toFixed()	It returns the string that represents a number with exact digits after a decimal point.
+
+// let x = 12.325235346342
+let x = 12
+
+let ans = x.toFixed(2)
+console.log("🚀 ~ ans:", ans)
+
+console.log("-=-=-=->", Number.isInteger(12.12));
+console.log("-=-=-=->", Number.parseFloat("12.12"));
+console.log("-=-=-=->", Number.parseInt("12.12"));
+console.log("-=-=-=->", +"12.12");
+
+console.log("----->", 12 + +"12");
+
+*/
+
