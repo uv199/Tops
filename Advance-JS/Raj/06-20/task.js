478a9b781b186d946b342aa8fd77253372bf5407
Urvish Patel <uspatel199@gmail.com>
2024-06-20 18:42:55 +0530

    add

---
+++ Advance-JS/Raj/06-20/task.js
@@ -0,0 +1,77 @@
+let no = 1290742387; //unique
+//  if there is pair of two same number repeate twise 656778733
+// if there is 4 number sequence available line 3211234435,94534567654 etc..
+// if any number is in trio 5734447343
+
+let school = [
+  {
+    name: "jay",
+    marks: [
+      {
+        subject: "hindi",
+        marks: 70,
+      },
+      {
+        subject: "gujarati",
+        marks: 68,
+      },
+      {
+        subject: "english",
+        marks: 81,
+      },
+      {
+        subject: "maths",
+        marks: 55,
+      },
+    ],
+  },
+  {
+    name: "jaymin",
+    marks: [
+      {
+        subject: "hindi",
+        marks: 77,
+      },
+      {
+        subject: "gujarati",
+        marks: 68,
+      },
+      {
+        subject: "english",
+        marks: 88,
+      },
+      {
+        subject: "maths",
+        marks: 36,
+      },
+    ],
+  },
+  {
+    name: "ravi",
+    marks: [
+      {
+        subject: "hindi",
+        marks: 90,
+      },
+      {
+        subject: "gujarati",
+        marks: 88,
+      },
+      {
+        subject: "english",
+        marks: 82,
+      },
+      {
+        subject: "maths",
+        marks: 63,
+      },
+    ],
+  },
+];
+
+//  who have higest scorrer in each subject =>
+let ans = [
+  { subject: "hindi", name: "ravi" },
+  { subject: "gujarati", name: "jaymin" },
+  // ...
+];
