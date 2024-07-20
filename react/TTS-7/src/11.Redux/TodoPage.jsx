5238cbbc5bf833894bd56a4be1cfdc1d10c832e5
Urvish Patel <uspatel199@gmail.com>
2024-07-20 09:40:14 +0530

    data

--- react/TTS-7/src/11.Redux/TodoPage.jsx
+++ react/TTS-7/src/11.Redux/TodoPage.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Plus } from "react-feather";
 import { useSelector, useDispatch } from "react-redux";
 import { Button, Input } from "reactstrap";
-import { addTask, updateTask ,deleteTask} from "./redux/todoSlice.js";
+import { addTask, updateTask, deleteTask,setData } from "./redux/todoSlice.js";
 
 export default function TodoPage() {
   let [todo, setTodo] = useState("");
   let [index, setIndex] = useState(null);
-  let [search,setSearch] = useState("");
-
-  const todoSlice = useSelector((store) => store.todoSlice);
-
+  let [search, setSearch] = useState("");
   const dispatch = useDispatch();
+  useEffect(()=>{    
+    dispatch(setData());
+  },[])
+
+  const todoData = useSelector((store) => {
+    return store.todoSlice.todoData;
+  });
+  const filteredItems = todoData.filter((item) =>
+    item.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const searhHandler = (e) => {
+    setSearch(e);
+  };
 
   const addHandler = () => {
     dispatch(addTask(todo));
@@ -26,16 +37,16 @@ export default function TodoPage() {
   const updateHandler = () => {
     let data = { index: index, newData: todo };
     dispatch(updateTask(data));
-    setIndex(null)
-    setTodo("")
+    setIndex(null);
+    setTodo("");
   };
 
-  const deleteHandler = (index) =>{
-    let confirmation = confirm('Do You want to delete..!');
-    if(confirmation){
+  const deleteHandler = (index) => {
+    let confirmation = confirm("Do You want to delete..!");
+    if (confirmation) {
       dispatch(deleteTask(index));
     }
-  }
+  };
   return (
     <div className="w-1/3 mt-5">
       <p className="mb-1">Enter a Task</p>
@@ -68,15 +79,15 @@ export default function TodoPage() {
       <div className="w-full border border-1 mt-4 p-4 rounded-1">
         <h1 className="text-center mb-0">Task</h1>
         <Input
-          className="shadow-none rounded-end-0"
+          className="shadow-none rounded-end-0 my-3"
           placeholder="Search here..."
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => searhHandler(e.target.value)}
           value={search}
         />
         <hr className="mt-0" />
-        {todoSlice.todoData.map((e, i) => {
+        {filteredItems?.map((e, i) => {
           return (
-            <div className="d-flex">
+            <div className="d-flex" key={i}>
               <div className=" border mt-2 px-2 py-1 rounded-2">{i + 1}.</div>
               <div className="flex justify-between w-100 border mt-2 px-2 py-1 rounded-2">
                 <h5>{e}</h5>
@@ -87,7 +98,12 @@ export default function TodoPage() {
                   >
                     Edit
                   </button>
-                  <button className="hover:!text-red-500" onClick={()=>deleteHandler(i)}>Delete</button>
+                  <button
+                    className="hover:!text-red-500"
+                    onClick={() => deleteHandler(i)}
+                  >
+                    Delete
+                  </button>
                 </div>
               </div>
             </div>
--- react/TTS-7/src/11.Redux/redux/todoSlice.js
+++ react/TTS-7/src/11.Redux/redux/todoSlice.js
@@ -5,20 +5,26 @@ const todoSlice = createSlice({
   initialState: { todoData: [] },
   reducers: {
     addTask: (state, action) => {
-      console.log("-----------  action----------->", action);
-      state.todoData.push(action.payload);
+        console.log("-----------  action----------->", action);
+        state.todoData.push(action.payload);        
+        localStorage.setItem('todoData', JSON.stringify(state.todoData));
     },
     updateTask: (state, action) => {
       console.log("-----------  action----------->", action);
-      //   state.todoData.push(action.payload);
       state.todoData.splice(action.payload.index,1,action.payload.newData)
+      localStorage.setItem('todoData', JSON.stringify(state.todoData));
     },
     deleteTask: (state,action)=>{
         state.todoData.splice(action.payload,1);
+        localStorage.setItem('todoData', JSON.stringify(state.todoData));
+    },
+    setData:(state,action)=>{
+        let todoTasks = JSON.parse(localStorage.getItem('todoData'));
+        state.todoData = todoTasks
     }
   },
 });
 
 export default todoSlice.reducer;
 
-export const { addTask, updateTask,deleteTask } = todoSlice.actions;
+export const { addTask, updateTask,deleteTask ,setData} = todoSlice.actions;
